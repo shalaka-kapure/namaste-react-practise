35fa4aee1b02aced54f78e2d3199f5b330a950b5
Shalaka <111489373+shalaka-kapure@users.noreply.github.com>
2023-05-01 13:50:34 +0530

    notes until lec 3

---
+++ App.js
@@ -0,0 +1,31 @@
+//Older way of writing React before JSX was invented
+
+// import React from "react";
+// import ReactDOM from "react-dom/client ";
+
+// const heading1 = React.createElement(
+//     "h2",
+//     {
+//         id: "title",
+//     },
+//     "Heading 1"
+// );
+
+// const heading2 = React.createElement(
+//     "h2",
+//     {
+//         id: "title",
+//     },
+//     "Heading 2"
+// );
+
+// const container = React.createElement(
+//     "div", 
+//     {
+//         id: "container",
+//     },
+//     [heading1, heading2]
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(container);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h2", {
+    id: "title"
+}, "Heading 1");
+const heading2 = React.createElement("h2", {
+    id: "title"
+}, "Heading 2");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
